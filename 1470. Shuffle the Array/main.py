e8c6086de1b547faa09cc5383405d7394baa8c44
Szymon Pluta <wenoxar@gmail.com>
2024-02-10 15:34:05 +0100

    1470, 2942

---
+++ 1470. Shuffle the Array/main.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        result = nums[:]
+        for i in range(n):
+            result[i * 2] = nums[i]
+            result[i * 2 + 1] = nums[n + i]
+        return result
