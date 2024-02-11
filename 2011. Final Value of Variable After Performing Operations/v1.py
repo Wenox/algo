0209b6bc3f885b53cab5d82c7962716d93913714
Szymon Pluta <wenoxar@gmail.com>
2024-02-11 15:58:41 +0100

    771, 1119, 2011

---
+++ 2011. Final Value of Variable After Performing Operations/v1.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def finalValueAfterOperations(self, operations: List[str]) -> int:
+        x = 0
+        for oper in operations:
+            if oper[0] == '+' or oper[2] == '+':
+                x += 1
+            else:
+                x -= 1
+        return x
