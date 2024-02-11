9a4f5ceb134dc2e06bab3d3b4e3abf3cae731c27
Szymon Pluta <wenoxar@gmail.com>
2024-02-11 01:48:46 +0100

    1108

---
+++ 1108. Defanging an IP Address/main.py
@@ -0,0 +1,13 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        # return address.replace('.', '[.]')
+        result = [''] * (len(address) + 6)
+        i = 0
+        for c in address:
+            if c == '.':
+                result[i:i + 3] = '[.]'
+                i += 3
+            else:
+                result[i] = c
+                i += 1
+        return ''.join(result)
