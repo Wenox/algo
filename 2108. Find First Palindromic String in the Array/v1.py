19de57adc6ee44cde19326421c4a9ee26303df54
Szymon Pluta <wenoxar@gmail.com>
2024-02-13 20:11:49 +0100

    383, 2108

---
+++ 2108. Find First Palindromic String in the Array/v1.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def is_palindrome(self, word: str) -> bool:
+        left, right = 0, len(word) - 1
+        while left < right:
+            if word[left] != word[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            if self.is_palindrome(word):
+                return word
+        return ""
+
+
--- 383. Ransom Note/v1.py
+++ 383. Ransom Note/v1.py
@@ -3,12 +3,17 @@ from collections import defaultdict
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine):
+            return False
+
         counts = defaultdict(int)
         for c in magazine:
             counts[c] += 1
+
         for c in ransomNote:
-            if c not in counts or counts[c] == 0:
+            if counts[c] == 0:
                 return False
             else:
                 counts[c] -= 1
+
         return True
