95f63fe218fd9aa77112c2cd4a89a37a52243ebc
Szymon Pluta <wenoxar@gmail.com>
2024-02-14 19:55:08 +0100

    485

---
+++ 485. Max Consecutive Ones/v1.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        max_count = 0
+        cur_count = 0
+        for n in nums:
+            cur_count = cur_count + 1 if n == 1 else 0
+            max_count = max(max_count, cur_count)
+
+        return max_count
