90dac3912ad551a7faa89277f8ad2343dd4f1dfb
Szymon Pluta <wenoxar@gmail.com>
2024-02-18 21:09:20 +0100

    9, 283

---
+++ 283. Move Zeroes/v1.py
@@ -0,0 +1,15 @@
+# Revisit: 1
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        seen_zeroes = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                seen_zeroes += 1
+            else:
+                nums[i - seen_zeroes] = nums[i]
+
+        for i in range(len(nums) - seen_zeroes, len(nums)):
+            nums[i] = 0
