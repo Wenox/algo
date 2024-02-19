fb88fd5b3e762d02058a7fa2fe1b7150c81c31b9
Szymon Pluta <wenoxar@gmail.com>
2024-02-19 17:03:29 +0100

    1051

---
+++ 1051. Height Checker/v1.py
@@ -0,0 +1,20 @@
+# Revisit: 1
+from typing import List
+
+
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        counts = [0] * 101
+        for height in heights:
+            counts[height] += 1
+
+        expected = []
+        for height, count in enumerate(counts):
+            if count != 0:
+                same_height_people = [height] * count
+                expected.extend(same_height_people)
+
+        diff = 0
+        for h1, h2 in zip(heights, expected):
+            if h1 != h2:
+                diff += 1
