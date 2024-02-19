bc3dcfaa1ca7044f2397508a2f12076cfb300bc3
Szymon Pluta <wenoxar@gmail.com>
2024-02-19 16:46:07 +0100

    905

---
+++ 905. Sort Array By Parity/v1.py
@@ -0,0 +1,12 @@
+# Revisit: 1
+from typing import List
+
+
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        odd = 0
+        for i in range(len(nums)):
+            if nums[i] % 2 == 0:
+                nums[odd], nums[i] = nums[i], nums[odd]
+                odd += 1
+        return nums
